41dfbb9b383ae4a726e723b333310c6cef5e9909
si200 <si200@users.noreply.github.com>
2025-09-28 14:21:46 +0100

    if/else commit

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -11,11 +11,19 @@ public static void guessTheNumber() {
     Scanner scanner = new Scanner(System.in);
     Random random = new Random(11);
 
+    int randomNum;
         
+        randomNum = random.nextInt(10) +1;
 
+        System.out.println("Guess a number between 1 and 10: ");
+        int guessNum = scanner.nextInt();
 
-
-
+    if (guessNum == randomNum) {
+            System.out.println("Correct! The Number was " + randomNum);
+    }
+    else {
+            System.out.println("Too low! Try again.");
+    }
 
 
 
