978e8e00b3f9d29a284153b69fa4c2e1514ff896
si200 <si200@users.noreply.github.com>
2025-09-28 13:53:17 +0100

    INIT Commit

---
+++ src/GuessTheNumber.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+
+/* Generate a random number between 1 and 10.
+ Ask user to guess until they get it right.
+ */
+
+public static void guessTheNumber() {
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random(11);
+
+        
+
+
+
+
+
+
+
+
+
+    scanner.close();
+}
+
+public static void main (String[] arg) {
+    guessTheNumber();
+}
+
+}
